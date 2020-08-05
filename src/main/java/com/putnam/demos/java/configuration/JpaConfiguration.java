d757d6acb95e1bdf4fa32cdb9eb0df7cbf180d66
Nathan <beachcoder@outlook.com>
2020-08-05 19:11:15 -0400

    Added configuration for JPA demo

--- src/main/java/com/putnam/demos/java/configuration/JpaConfiguration.java
+++ src/main/java/com/putnam/demos/java/configuration/JpaConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories(bootstrapMode = BootstrapMode.DEFAULT,basePackages = {"com.putnam.demos.java.domain","com.putnam.demos.java.repositories"},
 enableDefaultTransactions = true)
-@EnableTransactionManagement(mode = AdviceMode.PROXY)
+@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 public class JpaConfiguration {
-    
+
 }
